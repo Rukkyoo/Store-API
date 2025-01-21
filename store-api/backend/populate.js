9ff13108ef6a50663701f242485af443eda40840
Michael Omonedo <rukkyomonedo@gmail.com>
2025-01-21 19:39:37 +0100

    fixed bug in populate.js

--- store-api/backend/populate.js
+++ store-api/backend/populate.js
@@ -1,15 +1,15 @@
 import connectDB from "./db/connect.js";
 import Product from "./models/product.js";
-/* import jsonProducts from "./products.json"; */
-import dotenv from 'dotenv';
-dotenv.config();
-
-const uri = "mongodb+srv://rukevweomonedo:rukevweomonedo17@nodejsprojects.4hash.mongodb.net/STORE-API?retryWrites=true&w=majority&appName=NodejsProjects"
-
+import fs from "fs";
+const jsonProducts = JSON.parse(fs.readFileSync("./products.json", "utf-8"));
+import dotenv from "dotenv";
+dotenv.config({ path: "../.env" });
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI || uri);
+    await connectDB(process.env.MONGO_URI);
+    await Product.deleteMany(); // delete all products
+    await Product.create(jsonProducts);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error(error);
