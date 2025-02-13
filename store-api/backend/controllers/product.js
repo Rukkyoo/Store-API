9a02231632e64d393a3cff13766d0b79d3584b39
Michael Omonedo <rukkyomonedo@gmail.com>
2025-02-13 22:12:26 +0100

    zzz

--- store-api/backend/controllers/product.js
+++ store-api/backend/controllers/product.js
@@ -5,6 +5,6 @@ export async function getAllProductsStatic(req, res) {
   res.status(200).json({ products, nbHits: products.length });
 }
 export async function getAllProducts(req, res) {
-    console.log(req.query)
-  res.status(200).json({ msg: "products route" });
+  const products = await Product.find(req.query);
+  res.status(200).json({ products, nbHits: products.length });
 }
