445418b22e91288b845fe6bc0ebb7bce3a26ab23
Michael Omonedo <rukkyomonedo@gmail.com>
2025-02-14 15:39:44 +0100

    worked on search functionality

--- store-api/backend/controllers/product.js
+++ store-api/backend/controllers/product.js
@@ -5,6 +5,13 @@ export async function getAllProductsStatic(req, res) {
   res.status(200).json({ products, nbHits: products.length });
 }
 export async function getAllProducts(req, res) {
-  const products = await Product.find(req.query);
+  const { featured } = req.query
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+  console.log(queryObject);
+  const products = await Product.find(queryObject);
   res.status(200).json({ products, nbHits: products.length });
 }
