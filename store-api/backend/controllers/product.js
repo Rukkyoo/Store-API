edb10bb1aa361e2eebe78ee715da19b38a1a62c2
Michael Omonedo <rukkyomonedo@gmail.com>
2025-02-15 12:20:58 +0100

    updating search query parameter

--- store-api/backend/controllers/product.js
+++ store-api/backend/controllers/product.js
@@ -1,16 +1,27 @@
 import Product from "../models/product.js";
 
 export async function getAllProductsStatic(req, res) {
-  const products = await Product.find({ featured: true });
+  const products = await Product.find({ 
+    name: "Vase",
+   });
   res.status(200).json({ products, nbHits: products.length });
 }
 export async function getAllProducts(req, res) {
-  const { featured } = req.query
+  const { featured, company, name } = req.query;
   const queryObject = {};
 
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
   }
+
+  if (company) {
+    queryObject.company = company;
+  }
+
+  if (name) {
+    queryObject.name = name;
+  }
+
   console.log(queryObject);
   const products = await Product.find(queryObject);
   res.status(200).json({ products, nbHits: products.length });
