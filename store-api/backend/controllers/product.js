b9ad0634dfc35465401b299236c92e786f73337a
Michael Omonedo <rukkyomonedo@gmail.com>
2025-02-15 21:00:34 +0100

    sort query implemented

--- store-api/backend/controllers/product.js
+++ store-api/backend/controllers/product.js
@@ -1,14 +1,11 @@
 import Product from "../models/product.js";
 
 export async function getAllProductsStatic(req, res) {
-  const search = "bg";
-  const products = await Product.find({
-    name: { $regex: search, $options: "i" },
-  });
+  const products = await Product.find({}).sort("-name"); // Sort arranges the search results in descending order of the name field
   res.status(200).json({ products, nbHits: products.length });
 }
 export async function getAllProducts(req, res) {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -20,10 +17,17 @@ export async function getAllProducts(req, res) {
   }
 
   if (name) {
-    queryObject.name = { $regex: name, $options: "i" };
+    queryObject.name = { $regex: name, $options: "i" }; // Ensures there is no case sensitivity in the search
   }
 
-  console.log(queryObject);
-  const products = await Product.find(queryObject);
+  let result = Product.find(queryObject); // Searches the database based on the query object
+  if (sort) {
+    const sortList = sort.split(",").join(" "); // Converts the sort query to a string
+    result = result.sort(sortList); // Sorts the search results based on the sort query
+    console.log(sort);
+  } else {
+    result.sort("createdAt");
+  }
+  const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 }
